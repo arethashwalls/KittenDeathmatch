5ce984e897c009f5f41504c0e6e1447a6408968b
Aretha Walls <admin@arethawalls.com>
2018-11-07 13:20:21 -0700

    Fixed multiclick bug, added attacks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
             portrait: 'assets/images/black.jpg',
             health: 100,
             power: 20,
+            basePower: 20,
             counterPower: 30
         },
         {
@@ -13,6 +14,7 @@ $(document).ready(function () {
             portrait: 'assets/images/gray.jpg',
             health: 200,
             power: 15,
+            basePower: 15,
             counterPower: 25
         },
         {
@@ -20,6 +22,7 @@ $(document).ready(function () {
             portrait: 'assets/images/orange.jpg',
             health: 300,
             power: 5,
+            basePower: 5,
             counterPower: 15
         },
         {
@@ -27,6 +30,7 @@ $(document).ready(function () {
             portrait: 'assets/images/white.jpg',
             health: 80,
             power: 30,
+            basePower: 30,
             counterPower: 20
         }
     ];
@@ -38,6 +42,7 @@ $(document).ready(function () {
         $card.removeClass('blank-fighter-card hidden');
         //Give each card a custom ID based on the fighter's name:
         $card.attr('id', fighters[i].name.toLowerCase().replace(/\s/, "-") + '-card');
+        $card.attr('data-fighter-index', i);
         //Customize the card's content:
         $card.children('.fighter-name').text(fighters[i].name);
         $card.children('.fighter-portrait').attr('src', fighters[i].portrait);
@@ -48,33 +53,45 @@ $(document).ready(function () {
     var fighterPicked = false;
     var opponentPicked = false;
     var myFighterID = '';
+    var myOpponentId = '';
+    var myFighter;
+    var myOpponent;
 
-
-    $('.fighter-card').on('click', function () {
+    $('.fighter-card').unbind('click').click(function () {
         if (!fighterPicked) {
             $(this).appendTo('.fighter-box');
             fighterPicked = true;
             myFighterID = $(this).attr('id');
+            myFighter = fighters[$(this).attr('data-fighter-index')];
             $('.fighter-card').each(function () {
                 if ($(this).attr('id') !== myFighterID) {
                     $(this).appendTo('.opponent-selection-box');
                 }
             });
             $('.section-title').text('Chose Your Opponent');
+            return false;
         } else if (!opponentPicked) {
             $(this).appendTo('.opponent-box');
             opponentPicked = true;
             myOpponentId = $(this).attr('id');
+            myOpponent = fighters[$(this).attr('data-fighter-index')];
             $('.fighter-card').each(function () {
                 if ($(this).attr('id') !== myFighterID && $(this).attr('id') !== myOpponentId) {
                     $(this).addClass('hidden');
                 }
             });
             $('.section-title').text('Fight!');
+            return false;
         } else if(fighterPicked && opponentPicked) {
            if($(this).attr('id') === myFighterID) {
-               console.log('hi')
+                myOpponent.health -= myFighter.power;
+                $('#' + myOpponentId).children('.fighter-health').text(myOpponent.health);
+                myFighter.power *= myFighter.basePower;
+                myFighter.health -= myOpponent.counterPower;
+                $('#' + myFighterID).children('.fighter-health').text(myFighter.health);
            }
+           
+           return false;
         }
     });//End onClick for Fighter and Opponent selection
     
