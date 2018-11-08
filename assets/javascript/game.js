05c4a02cd125f179db53844f4f7c41ef7bf8ac8d
Aretha Walls <admin@arethawalls.com>
2018-11-08 14:54:33 -0700

    fussing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -31,11 +31,19 @@ $(document).ready(function () {
         }
     ];
 
+    function findMatchingIndex(id, copy) {
+        
+        for (let i = 0; i < copy.length; i++) {
+            if (copy[i].fighterID === id) return i;
+        }
+        return false;
+    }
+
     function fillBox(cards, box) {
         for (let i = 0; i < cards.length; i++) {
             var $card = $('.blank-fighter-card').clone();
             $card.removeClass('blank-fighter-card hidden');
-            $card.attr('data-fighter-index', i);
+            //$card.attr('data-fighter-index', i);
             $card.attr('id', cards[i].fighterID);
             //Customize the card's content:
             $card.children('.fighter-name').text(cards[i].name);
@@ -43,7 +51,6 @@ $(document).ready(function () {
             $card.children('.fighter-health').text(cards[i].health);
             $card.appendTo(box);
         }
-        console.log(box)
     }
 
     //This loop adds baseHealth and basePower to each fighter based on their starting values:
@@ -63,37 +70,56 @@ $(document).ready(function () {
     Match.prototype.setFighters = function () {
         $('.section-title').text('Chose Your Fighter!');
         fillBox(this.allFighters, $('.fighter-selection-box'));
-        
+
     }
 
     Match.prototype.setOpponents = function () {
+        
         $('.section-title').text('Chose Your Opponent!');
         fillBox(this.allOpponents, $('.opponent-selection-box'));
     }
 
     Match.prototype.setMyFighter = function (card) {
-        this.myFighter = this.allFighters[card.attr('data-fighter-index')];
-        this.allOpponents.splice(this.allOpponents.indexOf(this.myFighter), 1);
         
+        this.myFighter = this.allFighters[findMatchingIndex(card.attr('id'), this.allFighters)];
+        
+        this.allOpponents.splice(findMatchingIndex(this.myFighter.fighterID, this.allOpponents), 1);
         $('.fighter-box').append(card);
     }
 
     Match.prototype.setMyOpponent = function (card) {
-        this.myOpponent = this.allFighters[card.attr('data-fighter-index')];
-        this.allOpponents.splice(this.allOpponents.indexOf(this.myOpponent), 1);
+        
+        this.myOpponent = this.allFighters[findMatchingIndex(card.attr('id'), this.allFighters)];
+        
+        this.allOpponents.splice(findMatchingIndex(this.myFighter.fighterID, this.allOpponents), 1);
+        console.log(this.allOpponents)
         $('.opponent-box').append(card);
     }
 
-    Match.prototype.fight = function() {
+    Match.prototype.fight = function () {
+        
         this.myOpponent.health -= this.myFighter.power;
         this.myFighter.power *= this.myFighter.basePower;
         this.myFighter.health -= this.myOpponent.counterPower;
+
         $('#' + this.myOpponent.fighterID).children('.fighter-health').text(this.myOpponent.health);
         $('#' + this.myFighter.fighterID).children('.fighter-health').text(this.myFighter.health);
+  
     }
-    Match.prototype.lose = function() {
+    Match.prototype.lose = function () {
         $('.fighter-selection-box, .fighter-box, .opponent-selection-box, .opponent-box').empty();
         $('.win-loss-title').text('You lose...');
+    }
+
+    Match.prototype.win = function () {
+        $('.fighter-selection-box, .fighter-box, .opponent-selection-box, .opponent-box').empty();
+        $('.win-loss-title').text('You win!');
+    }
+
+    Match.prototype.removeOpponent = function (opponent) {
+
+        this.allOpponents.splice(findMatchingIndex(opponent, this.allOpponents), 1);
+        this.myOpponent = 0;
     }
 
     var match = new Match;
@@ -102,20 +128,39 @@ $(document).ready(function () {
 
     $('section').on('click', '.fighter-card', function () {
         if (match.myFighter === 0 && match.myOpponent === 0) {
+
             match.setMyFighter($(this));
             match.setOpponents();
             $('.fighter-selection-box').empty();
-        } else if (match.myOpponent === 0) {
+            return false;
+        } else if (match.myOpponent === 0 && $(this).attr('id') !== match.myFighter.fighterID) {
+
+
             match.setMyOpponent($(this));
+  
             $('.opponent-selection-box').empty();
+            return false;
         } else {
-            match.fight();
-            if(match.myFighter.health <= 0) {
-                match.lose();
-                window.setTimeout(function () {
-                    match = new Match;
-                    match.setFighters();
-                }, 3000);
+            if ($(this).attr('id') === match.myFighter.fighterID) {
+                
+                if (match.myFighter.health <= 0) {
+                    match.lose();
+                    window.setTimeout(function () {
+                        $('.win-loss-title').empty();
+                        match = new Match;
+                        match.setFighters();
+                    }, 3000);
+                } else if (match.myOpponent.health <= 0) {
+                    
+                    match.removeOpponent(match.myOpponent);
+                    $('.opponent-box').empty();
+                    match.setOpponents();
+                } else if (match.allOpponents.length === 0) {
+
+                } else {
+                    match.fight();
+                }
+                return false;
             }
         }
     });
