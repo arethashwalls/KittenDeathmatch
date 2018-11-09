67352aa11594fdf385a8efaae4ac4e475245327d
Aretha Walls <admin@arethawalls.com>
2018-11-09 10:11:26 -0700

    Initial display is working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -38,16 +38,31 @@ $(document).ready(function () {
         fighters[i].fighterID = fighters[i].name.toLowerCase().replace(/\s/, "-");
     }
 
-    //Deep copy the fighters array twice, once for fighters, once for opponents:
-    // let fighterChoices = $.extend(true, [], fighters);
-    // let opponentChoices = $.extend(true, [], fighters);
-
     //Define the Match object:
     function Match() {
         //Deep copy the fighters array twice, once for fighters, once for opponents:
         this.fighterChoices = $.extend(true, [], fighters);
         this.opponentChoices = $.extend(true, [], fighters);
+        this.myFighter = 0;
+        this.myOpponent = 0;
     }
+
+    Match.prototype.fillBox = function(cardArray) {
+        $box = $('<div>');
+        for(let i = 0; i < cardArray.length; i++) {
+            $card = $('.blank-fighter-card').clone();
+            $card.removeClass('blank-fighter-card hidden');
+            $card.attr('id', cardArray[i].fighterID);
+            $card.children('.fighter-name').text(cardArray[i].name);
+            $card.children('.fighter-portrait').attr('src', cardArray[i].portrait);
+            $card.children('.fighter-health').text(cardArray[i].health);
+            $box.append($card);
+        }
+        return $box;
+    }
+
+    var match = new Match;
+    $('.fighter-selection-box').replaceWith(match.fillBox(match.fighterChoices));
 
 
 
